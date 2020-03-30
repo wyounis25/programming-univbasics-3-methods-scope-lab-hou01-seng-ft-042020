811c5308563cd06abe83271944b557cecfc295ac
walid younis <walid.younis.90@gmail.com>
2020-03-30 12:19:14 +0000

    Automatically backed up by Learn

--- lib/catch_phrases.rb
+++ lib/catch_phrases.rb
@@ -1,9 +1,9 @@
 def mario
-  status = 'Thank You Mario! But Our Princess Is In Another Castle!'
+  $status = 'Thank You Mario! But Our Princess Is In Another Castle!'
   phrase = "It's-a me, Mario!"
   puts phrase
 end
 
 def toadstool
-  puts status
+  puts #{$status}
 end
